3dbc38830dc3678dd455b13c78019fe2cfb5900f
Marko Kjellgren <marko@Thickbook.local>
2026-07-29 22:06:06 +0200

    Move Hot right now below personal analytics

--- Jellyfin.Plugin.Jelana/Web/jelana.js
+++ Jellyfin.Plugin.Jelana/Web/jelana.js
@@ -1,7 +1,7 @@
 (() => {
     'use strict';
     const STYLE_ID = 'jelana-dashboard-styles';
-    const VERSION = '0.1.15.0';
+    const VERSION = '0.1.16.0';
     const embeddedInJellyfin = typeof window.ApiClient !== 'undefined';
     const basePath = embeddedInJellyfin
         ? ''
